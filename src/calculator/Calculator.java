95c30eb54b3d0e60a646d6b03ecd0cae7b07800a
ArtursVed <varturs81@gmail.com>
2018-08-23 20:29:38 +0300

    calculate two expression

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,59 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+
+        Double a = Double.parseDouble(expression[0]);
+        Double b = Double.parseDouble(expression[2]);
+
+
+        // int result = a + b;
+        double result;    // int - integer ; double- plavajusaja
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                return "error";
+
+            // vozvrasaet osibku, esli net  znaka
+        }
+        // esli vipolnjaem , tolko 2 deistvija , to pisem
+        if (expression.length < 4) {
+            return String.valueOf(result);
+        }
+        Double c = Double.parseDouble(expression[4]);
+        switch (expression[3]) {
+            case "+":
+                result = result + c;
+                break;
+            case "-":
+                result = result - c;
+                break;
+            case "*":
+                result = result * c;
+                break;
+            case "/":
+                result = result / c;
+                break;
+            default:
+                return "error";
+        }
+        return String.valueOf(result);
+
+        // valueOf -- preobrazuet iz cifr v teksts
+
     }
 
 }
