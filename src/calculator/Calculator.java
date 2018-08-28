e70f036112f113165810908a3a2f9a5e7f8aa99e
ArtursVed <varturs81@gmail.com>
2018-08-28 18:46:30 +0300

    calculate 28082018

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,79 +8,24 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-
-        Double a = Double.parseDouble(expression[0]);
-        Double b = Double.parseDouble(expression[2]);
-
-
-        // int result = a + b;
-        double result;    // int - integer ; double- plavajusaja
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                return "error";
-
-            // vozvrasaet osibku, esli net  znaka
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "error";
+            }
         }
-        // esli vipolnjaem , tolko 2 deistvija , to pisem
-        if (expression.length < 4) {
             return String.valueOf(result);
-        }
-        Double c = Double.parseDouble(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            default:
-                return "error";
-        }
-        if (expression.length < 6) {
-            return String.valueOf(result);
-        }
-        Double d = Double.parseDouble(expression[6]);
-        switch (expression[5]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            default:
-                return "error";
-        }
-        return String.valueOf(result);
 
-        // valueOf -- preobrazuet iz cifr v teksts
+        }
 
-    }
 
 }
